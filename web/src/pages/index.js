320feba570a7359631b32f9306be9a0c2f9ea282
Alcatraz Deve <alcatraz3019@gmail.com>
2023-01-27 03:20:34 +0100

    bug

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -3,10 +3,11 @@ import HeroSection from '../components/homepage/HeroSection';
 import SEO from '../components/SEO';
 import FeaturedBlogs from '../components/homepage/FeaturedBlogs';
 import TopCategories from '../components/homepage/TopCategories';
-import { Script } from 'gatsby';
+import  {isBrowser } from '../utils/isBrowser';
 
 const IndexPage = () => (
   <>
+  <isBrowser/>
     <SEO />
     <HeroSection />
     <div className="container">
