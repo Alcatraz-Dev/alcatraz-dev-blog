dd9667f7fe96ed1371b3fb3a4d026c1194b2e475
Alcatraz Deve <alcatraz3019@gmail.com>
2023-01-27 03:36:13 +0100

    fixed

--- web/src/constants/shear.js
+++ web/src/constants/shear.js
@@ -18,43 +18,43 @@ import { FacebookShareButton,
 
 
 
-function Share() {
-   const currentUrl = window.location.href;
+function Share({url,desc}) {
+  
   return (
     <div  style={{ marginTop:"20px"}}>
         <h2 style={{color:"#59981A" 
     }}>Please share this post ,thank's 😊 </h2>
       
 <div style={{alignItems:"center", justifyContent:"space-between"}}>
-<FacebookShareButton style={{ marginRight:"20px", marginTop:"20px"}} url={currentUrl} quote='Thanks for sharing this post 😊'
+<FacebookShareButton style={{ marginRight:"20px", marginTop:"20px"}} url={url} quote={desc}
         hashtag='#AlcatraDev'>
      <FacebookIcon size={35} round={true}/>
         </FacebookShareButton>
-        <FacebookMessengerShareButton style={{ marginRight:"20px", marginTop:"20px"}} url={currentUrl} quote='Thanks for sharing this post 😊'
+        <FacebookMessengerShareButton style={{ marginRight:"20px", marginTop:"20px"}} url={url} quote={desc}
         hashtag='#AlcatraDev'>
      <FacebookMessengerIcon size={35} round={true}/>
         </FacebookMessengerShareButton>
-        <TwitterShareButton style={{ marginRight:"20px", marginTop:"20px"}} url={currentUrl} quote='Thanks for sharing this post 😊'
+        <TwitterShareButton style={{ marginRight:"20px", marginTop:"20px"}} url={url} quote={desc}
         hashtag='#AlcatraDev'>
      <TwitterIcon size={35} round={true}/>
         </TwitterShareButton>
-        <LinkedinShareButton style={{ marginRight:"20px", marginTop:"20px"}} url={currentUrl} quote='Thanks for sharing this post 😊'
+        <LinkedinShareButton style={{ marginRight:"20px", marginTop:"20px"}} url={url} quote={desc}
         hashtag='#AlcatraDev'>
      <LinkedinIcon size={35} round={true} />
         </LinkedinShareButton>
-        <VKShareButton style={{ marginRight:"20px", marginTop:"20px"}} url={currentUrl} quote='Thanks for sharing this post 😊'
+        <VKShareButton style={{ marginRight:"20px", marginTop:"20px"}} url={url} quote={desc}
         hashtag='#AlcatraDev'>
      <VKIcon size={35} round={true} />
         </VKShareButton>
-        <TelegramShareButton style={{ marginRight:"20px", marginTop:"20px"}} url={currentUrl} quote='Thanks for sharing this post 😊'
+        <TelegramShareButton style={{ marginRight:"20px", marginTop:"20px"}} url={url} quote={desc}
         hashtag='#AlcatraDev'>
      <TelegramIcon size={35} round={true} />
         </TelegramShareButton>
-        <WhatsappShareButton style={{ marginRight:"20px", marginTop:"20px"}} url={currentUrl} quote='Thanks for sharing this post 😊'
+        <WhatsappShareButton style={{ marginRight:"20px", marginTop:"20px"}} url={url} quote={desc}
         hashtag='#AlcatraDev'>
      <WhatsappIcon size={35} round={true} />
         </WhatsappShareButton>
-        <RedditShareButton style={{ marginRight:"20px", marginTop:"20px"}} url={currentUrl} quote='Thanks for sharing this post 😊'
+        <RedditShareButton style={{ marginRight:"20px", marginTop:"20px"}} url={url} quote={desc}
         hashtag='#AlcatraDev'>
      <RedditIcon size={35} round={true} />
         </RedditShareButton>
